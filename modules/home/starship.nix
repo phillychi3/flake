05c79c0d6cfe0d0aa68e63bd398e10e929ab72a6
phillychi3 <phillychi3@gmail.com>
2026-04-06 13:41:08 +0800

    Fix starship format string escaping for tokyo-night theme

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -6,23 +6,9 @@
     settings = {
       "$schema" = "https://starship.rs/config-schema.json";
 
-      format = ''
-        [░▒▓](#a3aed2)\
-        [  ](bg:#a3aed2 fg:#090c0c)\
-        [](bg:#769ff0 fg:#a3aed2)\
-        $directory\
-        [](fg:#769ff0 bg:#394260)\
-        $git_branch\
-        $git_status\
-        [](fg:#394260 bg:#212736)\
-        $nodejs\
-        $rust\
-        $golang\
-        $php\
-        [](fg:#212736 bg:#1d2230)\
-        $time\
-        [ ](fg:#1d2230)\
-        \n$character'';
+      # Tokyo Night preset
+      # Each segment joined with \, newline at end via \n
+      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
 
       directory = {
         style = "fg:#e3e5e5 bg:#769ff0";
