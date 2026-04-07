ac778e99d1cb12b5577e3e269e1b9814a5dcbb6a
phillychi3 <phillychi3@gmail.com>
2026-04-08 01:33:33 +0800

    Add nix paths to fish shell

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -8,6 +8,9 @@
     '';
     shellInit = ''
       fish_add_path /Users/phillychi3/.local/bin
+      fish_add_path /etc/profiles/per-user/phillychi3/bin
+      fish_add_path /run/current-system/sw/bin
+      fish_add_path /nix/var/nix/profiles/default/bin
     '';
   };
 }
